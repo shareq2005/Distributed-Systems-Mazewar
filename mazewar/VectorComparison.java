ffdc5c96cb86d01e687ba08d6005e89388946b0e
syedshareq <syedshareq@a400a1c0-cd6a-45cb-af91-a587261bf1df>
2012-03-23 01:15:06 +0000

    Added an implementation of vector clocks from online.

---
+++ mazewar/VectorComparison.java
@@ -0,0 +1,10 @@
+/**
+ * Enumerates the four different outcomes of comparing two VectorClocks.
+ * 
+ * @author Frits de Nijs
+ * @author Peter Dijkshoorn
+ */
+public enum VectorComparison
+{
+	GREATER, EQUAL, SMALLER, SIMULTANEOUS
+}
